3e1c9589e0208caafdc2b7915a2b3f712650c6a5
shangzhongyue <shangzhongyue@cycfc.com>
2020-11-06 17:47:13 +0800

    ansible

--- demo/ad-hoc-tests.py
+++ demo/ad-hoc-tests.py
@@ -2,11 +2,18 @@ import sys
 from collections import namedtuple
 from optparse import Values
 
+# 核心类
+# 用于读取YAML和JSON格式的文件
 from ansible.parsing.dataloader import DataLoader
+# 用于存储各类变量信息
 from ansible.vars.manager import VariableManager
+# 用于导入资产文件
 from ansible.inventory.manager import InventoryManager
+# 操作单个主机信息
 from ansible.inventory.host import Host
+# 操作单个主机组信息
 from ansible.inventory.group import Group
+# 存储执行hosts的角色信息
 from ansible.playbook.play import Play
 from ansible.executor.task_queue_manager import TaskQueueManager
 from ansible.executor.playbook_executor import PlaybookExecutor

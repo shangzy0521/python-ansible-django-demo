7744347308bd8f627f0bdc3d53e212190afc74e9
shangzhongyue <shangzhongyue@cycfc.com>
2020-11-09 15:38:20 +0800

    ansible

--- demo/ad-hoc-tests.py
+++ demo/ad-hoc-tests.py
@@ -23,7 +23,7 @@ from ansible.executor.playbook_executor import PlaybookExecutor
 from ansible import context
 from ansible.module_utils.common.collections import ImmutableDict
 
-def ad_hoc(host,module,args):
+def ad_hoc(host,ssh_port,ssh_user,ssh_pwd,module,args):
     """
     ad-hoc 调用
     资产配置信息  这个是通过 InventoryManager和VariableManager 定义
@@ -34,10 +34,52 @@ def ad_hoc(host,module,args):
     :return:
     """
     host = host
+    ssh_port = ssh_port
+    ssh_user = ssh_user
+    ssh_pwd = ssh_pwd
     module = module
     args = args
 
-    # shangxiawen
+    # 上下文
+    connection = 'smart'  # 连接方式 local 本地方式，smart ssh方式
+    remote_user = None  # 远程用户
+    ack_pass = None  # 提示输入密码
+    sudo = None
+    sudo_user = None
+    ask_sudo_pass = None
+    module_path = None  # 模块路径，可以指定一个自定义模块的路径
+    become = None  # 是否提权
+    become_method = None  # 提权方式 默认 sudo 可以是 su
+    become_user = None  # 提权后，要成为的用户，并非登录用户
+    check = False
+    diff = False
+    listhosts = None
+    listtasks = None
+    listtags = None
+    verbosity = 3
+    syntax = None
+    start_at_task = None
+    inventory = None
+
+    context.CLIARGS = ImmutableDict(
+        connection=connection,
+        remote_user=remote_user,
+        ack_pass=ack_pass,
+        sudo=sudo,
+        sudo_user=sudo_user,
+        ask_sudo_pass=ask_sudo_pass,
+        module_path=module_path,
+        become=become,
+        become_method=become_method,
+        become_user=become_user,
+        check=check,
+        verbosity=verbosity,
+        listhosts=listhosts,
+        listtasks=listtasks,
+        listtags=listtags,
+        syntax=syntax,
+        start_at_task=start_at_task,
+    )
 
 
     # 资产配置信息
@@ -53,29 +95,15 @@ def ad_hoc(host,module,args):
     im.add_host(host=host)
 
     # 动态添加主机变量
-    vm.set_host_variable(host=host, varname="ansible_ssh_host", value='182.61.17.159')
-    vm.set_host_variable(host=host, varname="ansible_ssh_port", value=22)
-    vm.set_host_variable(host=host, varname="ansible_ssh_user", value='root')
-    vm.set_host_variable(host=host, varname="ansible_ssh_pass", value='Vinc08#22')
+    vm.set_host_variable(host=host, varname="ansible_ssh_host", value=host)
+    vm.set_host_variable(host=host, varname="ansible_ssh_port", value=ssh_port)
+    vm.set_host_variable(host=host, varname="ansible_ssh_user", value=ssh_user)
+    vm.set_host_variable(host=host, varname="ansible_ssh_pass", value=ssh_pwd)
 
-    # 此方式已不适用
-    # Options = namedtuple("Options", ["connection", "remote_user", "ask_sudo_pass", "verbosity", "ack_pass",
-    #                                  "module_path", "forks", "become", "become_method", "become_user", "check",
-    #                                  "listhosts", "listtasks", "listtags", "syntax", "sudo_user", "sudo", "diff"
-    # ])
-    # options = Options(connection='smart', remote_user=None, ack_pass=None, sudo_user=None, forks=5, sudo=None,
-    #                   ask_sudo_pass=False,verbosity=5, module_path=None, become=None, become_method=None,
-    #                   become_user=None, check=False, diff=False,listhosts=None, listtasks=None, listtags=None,
-    #                   syntax=None)
-
-    # 执行选项，这个类不是ansible的类，这个的功能就是为了构造参数
-    options = {'verbosity': 0, 'connection': 'smart', 'timeout': 60,}
-    ops = Values(options)
-    context._init_global_context(ops)
 
     # play的执行对象和模块，这里设置hosts，其实是因为play把play_source和资产信息关联后，执行的play的时候它会去资产信息中设置的sources的hosts文件中
     # 找你在play_source中设置的hosts是否在资产管理类里面。
-    play_source = dict(name="Ansible Play",  # 任务名称
+    play_source = dict(name="Ansible ad-hoc Play",  # 任务名称
                        hosts=host,  # 目标主机，可以填写具体主机也可以是主机组名称
                        gather_facts="no",  # 是否收集配置信息
                        # tasks是具体执行的任务，列表形式，每个具体任务都是一个字典
@@ -97,5 +125,5 @@ def ad_hoc(host,module,args):
     print(result)
 
 if __name__ == "__main__":
-    ad_hoc(host='182.61.17.159', module='shell', args='whoami')
-    ad_hoc(host='182.61.17.159', module='shell', args='ls /tmp')
+    ad_hoc(host='182.61.17.159', ssh_port=22, ssh_user='root', ssh_pwd='Vinc08#22', module='shell', args='whoami')
+    ad_hoc(host='182.61.17.159', ssh_port=22, ssh_user='root', ssh_pwd='Vinc08#22', module='shell', args='ls /tmp')

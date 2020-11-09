77ab63cbaa1878a7759679e41ed433eb4238a754
shangzhongyue <shangzhongyue@cycfc.com>
2020-11-09 10:42:16 +0800

    ansible

--- demo/ad-hoc-tests.py
+++ demo/ad-hoc-tests.py
@@ -26,21 +26,20 @@ dl = DataLoader()
 # loader= 表示是用什么方式来读取文件  sources=就是资产文件列表，里面可以是相对路径也可以是绝对路径
 # sources 如果为空会抛出一个WARNING 建议使用一个hosts文件 文件可以为空
 im = InventoryManager(loader=dl, sources=["hosts"])
-variable_manager = VariableManager(loader=dl, inventory=my_inventory)
-# my_inventory.add_group('mygroup')
-# my_group = Group(name='mygroup')
+# VariableManager类的调用方式
+vm = VariableManager(loader=dl, inventory=im)
 
-# my_host = Host(name='182.61.17.1599')
-# my_inventory.add_host(host='myhost', group='mygroup')
-my_inventory.add_host(host='myhost')
+# 动态添加主机
+im.add_host(host='dynamic_host')
 
-variable_manager.set_host_variable(host='myhost', varname="ansible_ssh_host", value='182.61.17.159')
-variable_manager.set_host_variable(host='myhost', varname="ansible_ssh_port", value=22)
-variable_manager.set_host_variable(host='myhost', varname="ansible_ssh_user", value='root')
-variable_manager.set_host_variable(host='myhost', varname="ansible_ssh_pass", value='Vinc08#22')
+# 动态添加主机变量
+host = im.get_host("dynamic_host")
+vm.set_host_variable(host=host, varname="ansible_ssh_host", value='182.61.17.159')
+vm.set_host_variable(host=host, varname="ansible_ssh_port", value=22)
+vm.set_host_variable(host=host, varname="ansible_ssh_user", value='root')
+vm.set_host_variable(host=host, varname="ansible_ssh_pass", value='Vinc08#22')
 
-# print(my_inventory.list_hosts())
-# print(variable_manager.get_vars())
+# 此方式已不适用
 # Options = namedtuple("Options", ["connection", "remote_user", "ask_sudo_pass", "verbosity", "ack_pass",
 #                                  "module_path", "forks", "become", "become_method", "become_user", "check",
 #                                  "listhosts", "listtasks", "listtags", "syntax", "sudo_user", "sudo", "diff"
@@ -49,17 +48,17 @@ variable_manager.set_host_variable(host='myhost', varname="ansible_ssh_pass", va
 #                   ask_sudo_pass=False,verbosity=5, module_path=None, become=None, become_method=None,
 #                   become_user=None, check=False, diff=False,listhosts=None, listtasks=None, listtags=None,
 #                   syntax=None)
-options = {'verbosity': 0, 'connection': 'smart', 'timeout': 30,}
+options = {'verbosity': 0, 'connection': 'smart', 'timeout': 60,}
 ops = Values(options)
 context._init_global_context(ops)
 
 play_source = dict(name="Ansible Play",hosts='myhost', gather_facts="no",
                    tasks=[dict(action=dict(module="shell", args="whoami"))])
 
-play = Play().load(play_source, variable_manager=variable_manager, loader=dl)
+play = Play().load(play_source, variable_manager=vm, loader=dl)
 passwords = dict()
 
-tqm = TaskQueueManager(inventory=my_inventory,variable_manager=variable_manager,loader=dl, passwords=passwords,)
+tqm = TaskQueueManager(inventory=im,variable_manager=vm,loader=dl, passwords=passwords,)
 
 
 result = tqm.run(play)
